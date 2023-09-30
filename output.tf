c2a4814427e158347b8193e1853892470de596d7
Tescuba <selamgualey@gmail.com>
2023-09-30 06:49:00 +0000

    #33 invalidate cloudfront stack when content version changes

--- output.tf
+++ output.tf
@@ -4,6 +4,11 @@ output "bucket_name" {
 }
 
 output "s3_website_endpoint" {
-  description = "S3 static website hosting endpoint"
+  description = "S3 Static Website hosting endpoint"
   value = module.terrahouse_aws.website_endpoint
+}
+
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
 }
